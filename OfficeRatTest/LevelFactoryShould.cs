c079402fa72986d70a001ccb3023328238b9c0b3
sheix <anton.sheihman@gmail.com>
2013-08-14 02:47:14 +0300

    Fixed test project location

---
+++ OfficeRatTest/LevelFactoryShould.cs
@@ -0,0 +1,22 @@
+﻿using Engine.Interfaces;
+using NUnit.Framework;
+
+namespace OfficeRatTest
+{
+    [TestFixture]
+    public class LevelFactoryShould
+    {
+        
+        [Test]
+        public void FillGrid()
+        {
+            var levelFactory = new LevelFactory();
+            IGrid grid = levelFactory.GenerateGrid();
+
+            Assert.NotNull(grid);
+        }
+
+        //[Test]
+        //public void
+    }
+}
