805cfb73b7d1622dc4828838ffdf0209469e456c
Anton Sheihman <anton.sheihman@gmail.com>
2014-06-19 14:39:57 +0300

    Work on calendar, add moons concept

--- Game/Calendar.cs
+++ Game/Calendar.cs
@@ -1,18 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Game
 {
     /// <summary>
-    /// There are 3 moons
-    /// 
+    /// There are 4 moons
+    /// There are 4 seasons, each one of 90 days
+    /// There are 3 months in each season
+    /// 30 days are length of each month
     /// </summary>
     public class Calendar
     {
-        public int Today;
+        public const int StartYear = 1328;
+        public const int DaysInYear = 360;
+
+        private static List<Moon> _moons = new List<Moon>{
+                                                  new Moon("Lun",27),
+                                                  new Moon("Mun",19),
+                                                  new Moon("Sput", 37)
+                                                  
+
+    };
+
+        public Calendar(int day = 1)
+        {
+            DayFromStart = day;
+            Year = StartYear + (day-1) / DaysInYear;
+        }
+
+        public int DayFromStart { get; private set; }
+        public int Year { get; private set; }
+
+        public int DayInYear { get { return DayFromStart - (Year - StartYear) * DaysInYear; } }
 
         public void NextDay()
         {
-            Today++;
+            DayFromStart++;
+            if ((DayFromStart - 1) % DaysInYear == 0) Year++;
+        }
+    }
+
+    internal class Moon
+    {
+        private readonly string _name;
+        private int _period;
+
+        public Moon(string name, int period)
+        {
+            _name = name;
+            _period = period;
         }
     }
 }
--- GameTest/CalendarShould.cs
+++ GameTest/CalendarShould.cs
@@ -9,10 +9,33 @@ namespace GameTest
         [Test]
         public void OnNextDayReturnDay()
         {
-            var calendar = new Calendar {Today = 25};
+            var calendar = new Calendar(25);;
             calendar.NextDay();
 
-            Assert.AreEqual(calendar.Today, 26);
+            Assert.AreEqual(calendar.DayFromStart, 26);
         }
+
+        [Test] 
+        public void FlipYearOnEvery360Days()
+        {
+            var calendar = new Calendar(360);
+            Assert.AreEqual(calendar.Year,Calendar.StartYear);
+            calendar.NextDay();
+            Assert.AreEqual(calendar.Year, Calendar.StartYear + 1);
+            Assert.AreEqual(calendar.DayInYear,1);
+        }
+
+        [Test()]
+        [TestCase(25,25)]
+        [TestCase(361,1)]
+        public void ReturnCorrectNumberOfDayInYear(int day, int dayinyear)
+        {
+            var calendar = new Calendar(day); 
+            
+            Assert.AreEqual(calendar.DayInYear,dayinyear);
+        }
+
+
+
     }
 }
